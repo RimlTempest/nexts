76a43a502e35fb5b3fe7c42e07586d297bc33711
RimlTempest <riml.slime@gmail.com>
2020-08-03 03:52:12 +0900

    MaterialUIのデバック環境でのスタイル読み込み失敗対策

---
+++ src/pages/_app.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { AppProps } from "next/app";
+import { ThemeProvider as StyledComponentsThemeProvider } from "styled-components";
+import { ThemeProvider as MaterialUIThemeProvider } from "@material-ui/core/styles";
+import { StylesProvider } from "@material-ui/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Layout from "../components/Layout";
+import theme from "../components/theme";
+
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  useEffect(() => {
+    const jssStyles: Element | null = document.querySelector(
+      "#jss-server-side"
+    );
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <StylesProvider injectFirst>
+      <MaterialUIThemeProvider theme={theme}>
+        <StyledComponentsThemeProvider theme={theme}>
+          <CssBaseline />
+          <Layout title="Nexts">
+            <Component {...pageProps} />
+          </Layout>
+        </StyledComponentsThemeProvider>
+      </MaterialUIThemeProvider>
+    </StylesProvider>
+  );
+};
+
+export default MyApp;
